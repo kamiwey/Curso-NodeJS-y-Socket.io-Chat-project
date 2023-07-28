f253b304013c94a43684a5d0e905ddac86602317
Kamiwey <kamiwey@gmail.com>
2023-07-28 12:44:30 +0200

    working

---
+++ client/main.js
@@ -0,0 +1,32 @@
+var socket = io('http://192.168.0.11:6677', { transports : ['websocket'] });
+
+socket.on('messages', function(data){
+    console.log(data);
+    render(data);
+});
+
+function render(data){
+    var html = data.map(function(message, index){
+        return (`
+            <div class="message">
+                <strong>${message.nickname}</strong>
+                <p>${message.text}</p>
+            </div>      
+        `);
+    }).join(' ');
+
+    var div_msgs = document.getElementById('messages');
+    div_msgs.innerHTML = html;
+    div_msgs.scrollTop = div_msgs.scrollHeight;
+};
+
+function addMessage(e){
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+    };
+
+    document.getElementById('nickname').style.display = 'none';
+    socket.emit('add-message', message);
+    return false;
+}
